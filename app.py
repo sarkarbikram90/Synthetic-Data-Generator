0fb0f1a1842fbb9b217f55cc6e703011bb6c9e53
Bikram Sarkar <sarkarbikram90@gmail.com>
2025-10-24 21:48:32 -0500

    update

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def show_landing_page():
     """, unsafe_allow_html=True)
     
     # Use Cases Section
-    # col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
     
     with col1:
         st.markdown("""
